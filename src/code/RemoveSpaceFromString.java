668a84f22ec20435d64d9be86fab0f193b08c1c2
Jagadheswaran <jagadhesjack88@gmail.com>
2026-02-03 22:13:50 +0530

    Added a program to remove whitespace from string

--- src/code/RemoveSpaceFromString.java
+++ src/code/RemoveSpaceFromString.java
@@ -6,9 +6,31 @@ public class RemoveSpaceFromString {
 
 		String input = "Automation Test Engineer";
 		
-		String stringAfterRemovedSpace = input.replaceAll("\\s", "");
+		String stringAfterRemovedSpace = input.replaceAll("\\s*", "");
 		
-		System.out.println("Final string after removing white-spaces: " + stringAfterRemovedSpace);
+//		* represent as regex quantifier, it checks and replace white-spaces that appears 0 or more times
+		
+//		More Regex Quantifiers / Metacharacters below
+//		Quantifier	Meaning	Example Matches
+//		X?	Appears 0 or 1 time	"a?" -> "", "a"
+//		X+	Appears 1 or more times	"a+" -> "a", "aa"
+//		X*	Appears 0 or more times	"a*" -> "", "a", "aa"
+//		X{n}	Appears exactly n times	"a{3}" -> "aaa"
+//		X{n,}	Appears at least n times	"a{2,}" -> "aa", "aaa"
+//		X{n,m}	Appears between n and m times	"a{2,4}" -> "aa", "aaa", "aaaa"
+		
+		System.out.println("Final String after Removing White-Spaces: " + stringAfterRemovedSpace);
+		
+//		Common Regex Patterns in Java
+//		. : Any character
+//		\d : Digit [0-9]
+//		\D : Non-digit
+//		\s : Whitespace
+//		\S : Non-whitespace
+//		\w : Word character [a-zA-Z0-9_]
+//		\W : Non-word character
+//		\b : Word boundary
+//		\B : Non-word boundary
 		
 	}
 
